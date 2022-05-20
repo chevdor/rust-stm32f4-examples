6cb98f429466680d3a893ea641d9027ba496ba8c
Chevdor <chevdor@gmail.com>
2022-05-20 23:46:49 +0200

    Fix blinky hal

--- blinky-hal/src/main.rs
+++ blinky-hal/src/main.rs
@@ -3,69 +3,65 @@
 
 extern crate cortex_m;
 extern crate cortex_m_rt;
-extern crate panic_abort;
 
-extern crate stm32f407g_disc as board;
 extern crate embedded_hal as hal;
+extern crate stm32f429i_disc as board;
 
 use cortex_m_rt::entry;
 
+use board::gpio;
+use board::gpio::gpiog::{PG13, PG14};
 use board::hal::delay::Delay;
 use board::hal::prelude::*;
 use board::hal::stm32;
-use board::gpio;
-use board::gpio::gpiod::{PD12, PD13, PD14, PD15};
-
-
-use hal::digital::OutputPin;
 
 use cortex_m::peripheral::Peripherals;
 
 struct Leds {
-   green:  PD12<gpio::Output<gpio::PushPull>>,
-   orange: PD13<gpio::Output<gpio::PushPull>>,
-   red:    PD14<gpio::Output<gpio::PushPull>>,
-   blue:   PD15<gpio::Output<gpio::PushPull>>,
+	green: PG13<gpio::Output<gpio::PushPull>>,
+	red: PG14<gpio::Output<gpio::PushPull>>,
 }
 
 #[entry]
 fn main() -> ! {
-    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
-        let gpiod = p.GPIOD.split();
+	if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+		let gpiog = p.GPIOG.split();
 
-        // Configure LED outputs  
-        let mut leds = Leds {
-          green: gpiod.pd12.into_push_pull_output(),
-          orange: gpiod.pd13.into_push_pull_output(),
-          red: gpiod.pd14.into_push_pull_output(),
-          blue: gpiod.pd15.into_push_pull_output(),
-        };
+		// Configure LED outputs
+		let mut leds = Leds { green: gpiog.pg13.into_push_pull_output(), red: gpiog.pg14.into_push_pull_output() };
 
-        // Constrain clock registers
-        let mut rcc = p.RCC.constrain();
+		// Constrain clock registers
+		let rcc = p.RCC.constrain();
 
-        // Configure clock to 168 MHz (i.e. the maximum) and freeze it
-        let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
+		// Configure clock to 168 MHz (i.e. the maximum) and freeze it
+		let clocks = rcc.cfgr.sysclk(168.mhz()).freeze();
 
-        // Get delay provider
-        let mut delay = Delay::new(cp.SYST, clocks);
+		// Get delay provider
+		let mut delay = Delay::new(cp.SYST, clocks);
+		let mut led = leds.red;
+		loop {
+			// Turn LED on
+			let _ = hal::digital::v2::OutputPin::set_high(&mut led);
 
-        loop {
-            // Turn LED on
-            leds.blue.set_high();
+			// Delay twice for half a second due to limited timer resolution
+			delay.delay_ms(50_u16);
+			// delay.delay_ms(500_u16);
+			// delay.delay_ms(500_u16);
 
-            // Delay twice for half a second due to limited timer resolution
-            delay.delay_ms(500_u16);
-            delay.delay_ms(500_u16);
+			// Turn LED off
+			let _ = hal::digital::v2::OutputPin::set_low(&mut led);
 
-            // Turn LED off
-            leds.blue.set_low();
+			// Delay twice for half a second due to limited timer resolution
+			delay.delay_ms(50_u16);
+			// delay.delay_ms(500_u16);
+			// delay.delay_ms(500_u16);
+		}
+	}
 
-            // Delay twice for half a second due to limited timer resolution
-            delay.delay_ms(500_u16);
-            delay.delay_ms(500_u16);
-        }
-    }
+	loop {}
+}
 
-    loop {}
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+	loop {}
 }
