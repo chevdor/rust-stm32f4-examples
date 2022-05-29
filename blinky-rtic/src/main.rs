46a4ea7fe4ea1f57885c608c4403646a6f6ccdf0
Chevdor <chevdor@gmail.com>
2022-05-30 00:11:42 +0200

    WIP

--- blinky-rtic/src/main.rs
+++ blinky-rtic/src/main.rs
@@ -8,13 +8,9 @@ use panic_halt as _;
 #[rtic::app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
 	use fugit::Duration;
-	use stm32f4xx_hal::gpio::gpioa::PA0;
 	use stm32f4xx_hal::gpio::gpiog::{PG13, PG14};
 	use stm32f4xx_hal::gpio::*;
-	use stm32f4xx_hal::{
-		gpio::{Edge, Input, Output, PushPull},
-		prelude::*,
-	};
+	use stm32f4xx_hal::gpio::{Output, PushPull};
 	use systick_monotonic::Systick;
 
 	#[shared]
@@ -22,7 +18,6 @@ mod app {
 
 	#[local]
 	struct Local {
-		button: PA0<Input<PullDown>>,
 		led1: PG13<Output<PushPull>>,
 		led2: PG14<Output<PushPull>>,
 		state: bool,
@@ -32,25 +27,18 @@ mod app {
 	type MonoTimer = Systick<1000>;
 
 	#[init]
-	fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
-		let mut syscfg = ctx.device.SYSCFG.constrain();
-
+	fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
 		let gpiog = ctx.device.GPIOG.split();
 		let led1 = gpiog.pg13.into_push_pull_output();
 		let led2 = gpiog.pg14.into_push_pull_output();
 		let mono = Systick::new(ctx.core.SYST, 36_000_000);
 
-		let gpioa = ctx.device.GPIOA.split();
-		let mut button = gpioa.pa0.into_pull_down_input();
-		button.make_interrupt_source(&mut syscfg);
-		button.enable_interrupt(&mut ctx.device.EXTI);
-		button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
 		tick::spawn().unwrap();
 		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(100)).unwrap();
-		(Shared {}, Local { button, led1, led2, state: true }, init::Monotonics(mono))
+		(Shared {}, Local { led1, led2, state: true }, init::Monotonics(mono))
 	}
 
-	#[task(local = [button, led1])]
+	#[task(local = [led1])]
 	fn blink1(cx: blink1::Context) {
 		cx.local.led1.toggle();
 		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(50)).unwrap();
--- button-led-rtic/src/main.rs
+++ button-led-rtic/src/main.rs
@@ -1,33 +1,30 @@
 #![deny(unsafe_code)]
-// #![deny(warnings)]
-#![no_main]
+#![deny(warnings)]
 #![no_std]
+#![no_main]
 
-extern crate stm32f4xx_hal as board;
+use panic_halt as _;
 
-use cortex_m::peripheral::Peripherals;
-use panic_rtt_target as _;
-use rtic::app;
-use rtt_target::{rprintln, rtt_init_print};
-use stm32f4::stm32f429;
-use stm32f429i_disc::hal::stm32;
-use stm32f4xx_hal::gpio::{gpiog::PG13, Output, PushPull};
-use stm32f4xx_hal::prelude::*;
-use systick_monotonic::{fugit::Duration, Systick};
-
-#[app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+#[rtic::app(device = stm32f4xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
-	use super::*;
-	// use crate::app::OutputPin;
-	// use embedded_hal::digital::v2::OutputPin;
-	// use stm32f429i_disc::gpio::GpioExt;
+	use fugit::Duration;
+	use stm32f4xx_hal::gpio::gpioa::PA0;
+	use stm32f4xx_hal::gpio::gpiog::{PG13, PG14};
+	use stm32f4xx_hal::gpio::*;
+	use stm32f4xx_hal::{
+		gpio::{Edge, Input, Output, PushPull},
+		prelude::*,
+	};
+	use systick_monotonic::Systick;
 
 	#[shared]
 	struct Shared {}
 
 	#[local]
 	struct Local {
-		// led: PG13<Output<PushPull>>,
+		button: PA0<Input<PullDown>>,
+		led1: PG13<Output<PushPull>>,
+		led2: PG14<Output<PushPull>>,
 		state: bool,
 	}
 
@@ -35,42 +32,45 @@ mod app {
 	type MonoTimer = Systick<1000>;
 
 	#[init]
-	fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
-		let p = stm32::Peripherals::take().unwrap();
+	fn init(mut ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+		let mut syscfg = ctx.device.SYSCFG.constrain();
 
-		// Setup clocks
-		// let mut flash = cx.device.FLASH.constrain();
-		let mut rcc = cx.device.RCC.constrain();
+		let gpiog = ctx.device.GPIOG.split();
+		let led1 = gpiog.pg13.into_push_pull_output();
+		let led2 = gpiog.pg14.into_push_pull_output();
+		let mono = Systick::new(ctx.core.SYST, 36_000_000);
 
-		let mono = Systick::new(cx.core.SYST, 36_000_000);
-
-		rtt_init_print!();
-		rprintln!("init");
-
-		let mhz = 1_000_000;
-		// let _clocks = rcc.cfgr.use_hse(8 * mhz).sysclk(36 * mhz).pclk1(36 * mhz).freeze(&mut flash.acr);
-
-		// Setup LED
-		// let gpiog = p.GPIOG.split();
-		// let mut led = gpiog.pg13.into_push_pull_output();
-
-		// led.set_high();
-
-		// Schedule the blinking task
-		blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
-		return (Shared {}, Local { state: false }, init::Monotonics(mono));
+		let gpioa = ctx.device.GPIOA.split();
+		let mut button = gpioa.pa0.into_pull_down_input();
+		button.make_interrupt_source(&mut syscfg);
+		button.enable_interrupt(&mut ctx.device.EXTI);
+		button.trigger_on_edge(&mut ctx.device.EXTI, Edge::Falling);
+		tick::spawn().unwrap();
+		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(100)).unwrap();
+		(Shared {}, Local { button, led1, led2, state: true }, init::Monotonics(mono))
 	}
 
-	#[task(local = [state])]
-	fn blink(cx: blink::Context) {
-		rprintln!("blink");
+	#[task(local = [led1])]
+	fn blink1(cx: blink1::Context) {
+		cx.local.led1.toggle();
+		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(50)).unwrap();
+	}
+
+	#[task(local = [led2, state])]
+	fn tick(cx: tick::Context) {
 		if *cx.local.state {
-			// cx.local.led.set_high();
+			cx.local.led2.set_high();
 			*cx.local.state = false;
 		} else {
-			// cx.local.led.set_low();
+			cx.local.led2.set_low();
 			*cx.local.state = true;
 		}
-		blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+		tick::spawn_after(Duration::<u64, 1, 1000>::from_ticks(55)).unwrap();
+	}
+
+	#[task(local = [button, led1])]
+	fn btn_1(cx: blink1::Context) {
+		cx.local.led1.toggle();
+		blink1::spawn_after(Duration::<u64, 1, 1000>::from_ticks(50)).unwrap();
 	}
 }
